9dcde8a867a50bab935968ba9eaf77a03fa93ba2
Ihor Navrotskyj <i.navrotskyj@webitel.com>
2023-05-03 12:25:07 +0300

    WTEL-3409

--- proto.cmake
+++ proto.cmake
@@ -1,33 +1,33 @@
 
-file(MAKE_DIRECTORY ${GENERATED_PROTOBUF_PATH})
-set(PROTO_PATH "${CMAKE_SOURCE_DIR}/src/protos")
+file(MAKE_DIRECTORY ${WBT_GENERATED_PROTOBUF_PATH})
+set(WBT_PROTO_PATH "${CMAKE_SOURCE_DIR}/src/protos")
 
-file(GLOB_RECURSE proto_files ABSOLUTE ${PROTO_PATH}/*.proto)
+file(GLOB_RECURSE wbt_proto_files ABSOLUTE ${WBT_PROTO_PATH}/*.proto)
 
 # Generate Protobuf cpp sources
-set(PROTO_HDRS)
-set(PROTO_SRCS)
-foreach(PROTO_FILE IN LISTS proto_files)
-    #message(STATUS "protoc proto(cc): ${PROTO_FILE}")
-#    get_filename_component(PROTO_DIR ${PROTO_FILE} DIRECTORY)
-    get_filename_component(PROTO_NAME ${PROTO_FILE} NAME_WE)
-    set(PROTO_HDR ${GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.pb.h)
-    set(PROTO_SRC ${GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.pb.cc)
-    set(PROTO_GRPC_HDR ${GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.grpc.pb.h)
-    set(PROTO_GRPC_SRC ${GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.grpc.pb.cc)
-#    message(STATUS "protoc hdr: ${PROTO_HDR}")
-#    message(STATUS "protoc src: ${PROTO_SRC}")
+set(WBT_PROTO_HDRS)
+set(WBT_PROTO_SRCS)
+foreach(WBT_PROTO_FILE IN LISTS wbt_proto_files)
+    #message(STATUS "protoc proto(cc): ${WBT_PROTO_FILE}")
+#    get_filename_component(PROTO_DIR ${WBT_PROTO_FILE} DIRECTORY)
+    get_filename_component(PROTO_NAME ${WBT_PROTO_FILE} NAME_WE)
+    set(WBT_PROTO_HDR ${WBT_GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.pb.h)
+    set(WBT_PROTO_SRC ${WBT_GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.pb.cc)
+    set(WBT_PROTO_GRPC_HDR ${WBT_GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.grpc.pb.h)
+    set(WBT_PROTO_GRPC_SRC ${WBT_GENERATED_PROTOBUF_PATH}/${PROTO_NAME}.grpc.pb.cc)
+    message(STATUS "protoc hdr: ${WBT_PROTO_HDR}")
+    message(STATUS "protoc src: ${WBT_PROTO_SRC}")
     add_custom_command(
-            OUTPUT ${PROTO_SRC} ${PROTO_HDR} ${PROTO_GRPC_SRC} ${PROTO_GRPC_HDR}
+            OUTPUT ${WBT_PROTO_SRC} ${WBT_PROTO_HDR} ${WBT_PROTO_GRPC_SRC} ${WBT_PROTO_GRPC_HDR}
             COMMAND ${_PROTOBUF_PROTOC}
-            ARGS --grpc_out "${GENERATED_PROTOBUF_PATH}"
-            --cpp_out "${GENERATED_PROTOBUF_PATH}"
-            -I "${PROTO_PATH}"
+            ARGS --grpc_out "${WBT_GENERATED_PROTOBUF_PATH}"
+            --cpp_out "${WBT_GENERATED_PROTOBUF_PATH}"
+            -I "${WBT_PROTO_PATH}"
             --plugin=protoc-gen-grpc="${_GRPC_CPP_PLUGIN_EXECUTABLE}"
-            "${PROTO_FILE}"
-            DEPENDS "${PROTO_FILE}")
-    list(APPEND PROTO_HDRS ${PROTO_HDR})
-    list(APPEND PROTO_HDRS ${PROTO_GRPC_HDR})
-    list(APPEND PROTO_SRCS ${PROTO_SRC})
-    list(APPEND PROTO_SRCS ${PROTO_GRPC_SRC})
+            "${WBT_PROTO_FILE}"
+            DEPENDS "${WBT_PROTO_FILE}" USES_TERMINAL)
+    list(APPEND WBT_PROTO_HDRS ${WBT_PROTO_HDR})
+    list(APPEND WBT_PROTO_HDRS ${WBT_PROTO_GRPC_HDR})
+    list(APPEND WBT_PROTO_SRCS ${WBT_PROTO_SRC})
+    list(APPEND WBT_PROTO_SRCS ${WBT_PROTO_GRPC_SRC})
 endforeach()
